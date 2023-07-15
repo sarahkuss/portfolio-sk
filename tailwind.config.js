960059d72b6222a7f289a4d1f2745b9684b2984c
Sarah Kuss <sarah.kuss33@gmail.com>
2023-07-14 22:04:05 -0400

    added background

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,14 @@ module.exports = {
   ],
   theme: {
     extend: {
+      borderColor: {
+        'orange1': '#ff00cc',
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'plant-background': "url(/images/darkbgplant.jpg)"
       },
     },
   },
