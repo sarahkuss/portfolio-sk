ffa3dc79798e8d81e3ea8cd7838372da95098404
Sarah Kuss <sarah.kuss33@gmail.com>
2023-07-14 22:06:06 -0400

    changed background color

--- components/Projects.js
+++ components/Projects.js
@@ -1,6 +1,6 @@
 export default function Projects() {
   return (
-    <section className="text-gray-600 body-font">
+    <section className="text-gray-600 body-font bg-orange-300 bg-opacity-90">
       <div className="container px-5 py-24 mx-auto">
         <div className="flex flex-wrap w-full mb-20">
           <div className="lg:w-1/2 w-full mb-6 lg:mb-0">
