a87467cc09e3796e64874bb2acfa2163810209c4
Sarah Kuss <sarah.kuss33@gmail.com>
2023-08-09 11:36:33 -0400

    fixed projects margin, still need to fix for mobile view

--- components/Projects.js
+++ components/Projects.js
@@ -1,13 +1,13 @@
 export default function Projects() {
   return (
     <section className="text-gray-600 body-font bg-orange-300 bg-opacity-90">
-      <div className="container px-5 py-24 mx-auto">
-        <div className="flex flex-wrap w-full mb-20">
-          <div className="lg:w-1/2 w-full mb-6 lg:mb-0">
+      <div className="container px-5 py-10 mx-auto">
+        <div className="flex flex-wrap w-full mb-5">
+          <div className="lg:w-1/2 w-full mb-0 lg:mb-0">
             <h1 className="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">
               Projects
             </h1>
-            <div className="h-1 w-20 bg-indigo-500 rounded"></div>
+            <div className="h-1 w-20 bg-orange-700 rounded"></div>
           </div>
           {/* <p className="lg:w-1/2 w-full leading-relaxed text-gray-500">
             Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical
