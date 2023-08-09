5679eab902aac93fb014cb17f6ac5c70493da372
Sarah Kuss <sarah.kuss33@gmail.com>
2023-08-09 11:20:27 -0400

    added some projects

--- app/page.js
+++ app/page.js
@@ -4,9 +4,9 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-plant-background bg-center">
+    <main className="flex min-h-screen flex-col items-center justify-between p-20 bg-plant-background bg-center">
       <Hero />
-      {/* <Projects /> */}
+      <Projects />
     </main>
   )
 }
--- components/Projects.js
+++ components/Projects.js
@@ -5,68 +5,108 @@ export default function Projects() {
         <div className="flex flex-wrap w-full mb-20">
           <div className="lg:w-1/2 w-full mb-6 lg:mb-0">
             <h1 className="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">
-              Pitchfork Kickstarter Taxidermy
+              Projects
             </h1>
             <div className="h-1 w-20 bg-indigo-500 rounded"></div>
           </div>
-          <p className="lg:w-1/2 w-full leading-relaxed text-gray-500">
+          {/* <p className="lg:w-1/2 w-full leading-relaxed text-gray-500">
             Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical
             gentrify, subway tile poke farm-to-table. Franzen you probably
             haven't heard of them man bun deep jianbing selfies heirloom prism
             food truck ugh squid celiac humblebrag.
-          </p>
+          </p> */}
         </div>
         <div className="flex flex-wrap -m-4">
           <div className="xl:w-1/4 md:w-1/2 p-4">
             <div className="bg-gray-100 p-6 rounded-lg">
-              <img
-                className="h-40 rounded w-full object-cover object-center mb-6"
-                src="https://dummyimage.com/720x400"
-                alt="content"
-              />
-              <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">
+              <a href="https://conservationconnect.net" target="_blank" rel="noreferrer">
+                <img
+                  className="h-40 rounded w-full object-cover object-center mb-6"
+                  src="/images/CC.png"
+                  alt="content"
+                />
+              </a>
+              {/* <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">
                 SUBTITLE
-              </h3>
+              </h3> */}
               <h2 className="text-lg text-gray-900 font-medium title-font mb-4">
-                Chichen Itza
+                Conservation Connect
               </h2>
               <p className="leading-relaxed text-base">
-                Fingerstache flexitarian street art 8-bit waistcoat. Distillery
-                hexagon disrupt edison bulbche.
+                Full Stack React Web App built using MERN stack and deployed in AWS.
+              </p>
+            </div>
+          </div>
+          <div className="xl:w-1/4 md:w-1/2 p-4">
+            <div className="bg-gray-100 p-6 rounded-lg">
+              <a href="https://janssenpartners.com" target="_blank" rel="noreferrer">
+                <img
+                  className="h-40 rounded w-full object-cover object-center mb-6"
+                  src="/images/jp.png"
+                  alt="content"
+                />
+              </a>
+              {/* <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">
+                SUBTITLE
+              </h3> */}
+              <h2 className="text-lg text-gray-900 font-medium title-font mb-4">
+                Janssen Partners
+              </h2>
+              <p className="leading-relaxed text-base">
+                WordPress website built for Janssen Partners using plugins and custom themes.
+              </p>
+            </div>
+          </div>
+          <div className="xl:w-1/4 md:w-1/2 p-4">
+            <div className="bg-gray-100 p-6 rounded-lg">
+              <a href="https://mpa-coaching-website.web.app/" target="_blank" rel="noreferrer">
+                <img
+                  className="h-40 rounded w-full object-cover object-center mb-6"
+                  src="/images/mpa.png"
+                  alt="content"
+                />
+              </a>
+              {/* <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">
+                SUBTITLE
+              </h3> */}
+              <h2 className="text-lg text-gray-900 font-medium title-font mb-4">
+                Mary Pat Adams- Coaching
+              </h2>
+              <p className="leading-relaxed text-base">
+                React Website styled using Bootstrap and deployed to GCP.
               </p>
             </div>
           </div>
           <div className="xl:w-1/4 md:w-1/2 p-4">
             <div className="bg-gray-100 p-6 rounded-lg">
               <img
-                className="h-40 rounded w-full object-cover object-center mb-6"
-                src="https://dummyimage.com/721x401"
+                className="h-40 rounded w-full object-center mb-6"
+                src="/images/water.jpg"
                 alt="content"
               />
-              <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">
+              {/* <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">
                 SUBTITLE
-              </h3>
+              </h3> */}
               <h2 className="text-lg text-gray-900 font-medium title-font mb-4">
-                Colosseum Roma
+                Water Tracker
               </h2>
               <p className="leading-relaxed text-base">
-                Fingerstache flexitarian street art 8-bit waistcoat. Distillery
-                hexagon disrupt edison bulbche.
+                React Native Mobile Application styled using Native Base.
               </p>
             </div>
           </div>
-          <div className="xl:w-1/4 md:w-1/2 p-4">
+          {/* <div className="xl:w-1/4 md:w-1/2 p-4">
             <div className="bg-gray-100 p-6 rounded-lg">
               <img
                 className="h-40 rounded w-full object-cover object-center mb-6"
-                src="https://dummyimage.com/722x402"
+                src="https://dummyimage.com/723x403"
                 alt="content"
               />
               <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">
                 SUBTITLE
               </h3>
               <h2 className="text-lg text-gray-900 font-medium title-font mb-4">
-                Great Pyramid of Giza
+                San Francisco
               </h2>
               <p className="leading-relaxed text-base">
                 Fingerstache flexitarian street art 8-bit waistcoat. Distillery
@@ -93,6 +133,44 @@ export default function Projects() {
               </p>
             </div>
           </div>
+          <div className="xl:w-1/4 md:w-1/2 p-4">
+            <div className="bg-gray-100 p-6 rounded-lg">
+              <img
+                className="h-40 rounded w-full object-cover object-center mb-6"
+                src="https://dummyimage.com/723x403"
+                alt="content"
+              />
+              <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">
+                SUBTITLE
+              </h3>
+              <h2 className="text-lg text-gray-900 font-medium title-font mb-4">
+                San Francisco
+              </h2>
+              <p className="leading-relaxed text-base">
+                Fingerstache flexitarian street art 8-bit waistcoat. Distillery
+                hexagon disrupt edison bulbche.
+              </p>
+            </div>
+          </div>
+          <div className="xl:w-1/4 md:w-1/2 p-4">
+            <div className="bg-gray-100 p-6 rounded-lg">
+              <img
+                className="h-40 rounded w-full object-cover object-center mb-6"
+                src="https://dummyimage.com/723x403"
+                alt="content"
+              />
+              <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">
+                SUBTITLE
+              </h3>
+              <h2 className="text-lg text-gray-900 font-medium title-font mb-4">
+                San Francisco
+              </h2>
+              <p className="leading-relaxed text-base">
+                Fingerstache flexitarian street art 8-bit waistcoat. Distillery
+                hexagon disrupt edison bulbche.
+              </p>
+            </div>
+          </div> */}
         </div>
       </div>
     </section>
